d7bdc4058703c272addb318a2eec8e2e590fc8b7
greatwmc <miles.gwood@gmail.com>
2016-04-08 16:15:33 -0400

    Initial Person Class Added- don't know if i'll be needing it since I am using SQL lite.

---
+++ ContactEntryDesktop/src/Person.java
@@ -0,0 +1,15 @@
+
+public class Person {
+	int age;
+	char sex;
+	String name;
+	String location;
+	String comments;
+	String phone_number;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
